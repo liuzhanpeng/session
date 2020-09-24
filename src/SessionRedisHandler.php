b5694c0c88dc4914923f17e267e5f7417daf00f3
liuzhanpeng <liuzhanpeng@gmail.com>
2020-09-24 17:11:39 +0800

    增加Redis支持

---
+++ src/SessionRedisHandler.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace EasySwoole\Session;
+
+use EasySwoole\RedisPool\Redis;
+
+class SessionRedisHandler implements \SessionHandlerInterface
+{
+    private $redisPool;
+    private $lifeTime;
+
+    public function __construct(Redis $pool, int $lifeTime = 3600)
+    {
+        $this->redisPool = $pool;
+        $this->lifeTime = $lifeTime;
+    }
+
+    public function close()
+    {
+        return true;
+    }
+
+    public function destroy($session_id)
+    {
+        return $this->redisPool::defer('redis')->del($session_id);
+    }
+
+    public function gc($maxlifetime)
+    {
+        //空实现
+    }
+
+    public function open($save_path, $name)
+    {
+        return true;
+    }
+
+    public function read($session_id)
+    {
+        return $this->redisPool::defer('redis')->get($session_id);
+    }
+
+    public function write($session_id, $session_data)
+    {
+        return $this->redisPool::defer('redis')->set($session_id, $session_data, $this->lifeTime);
+    }
+}
